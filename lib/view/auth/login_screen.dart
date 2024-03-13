3cf0e1bd049e2b596c900cf82c9c45e7c5bb6d3e
Shahabmustafa <shahabmustafa57@gmail.com>
2024-03-13 21:40:37 +0500

    add login screen

--- lib/view/auth/login_screen.dart
+++ lib/view/auth/login_screen.dart
@@ -1,9 +1,11 @@
+import 'package:digital_attendance/provider/loading_button_provider.dart';
+import 'package:digital_attendance/view/auth/information_screen.dart';
+import 'package:digital_attendance/view/auth/sign_up_screen.dart';
+import 'package:digital_attendance/viewmodel/auth/auth_controller.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:safi_shoes_admin/res/widget/app_button.dart';
-import 'package:safi_shoes_admin/res/widget/app_text_field.dart';
-import 'package:safi_shoes_admin/view/auth/sign_up_screen.dart';
-import 'package:safi_shoes_admin/viewmodel/auth/auth_controller.dart';
+import 'package:provider/provider.dart';
+import '../../res/component/app_button.dart';
+import '../../res/component/app_text_field.dart';
 
 
 class LoginScreen extends StatefulWidget {
@@ -14,7 +16,8 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  final login = Get.put(AuthenticationController());
+  final email = TextEditingController();
+  final password = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,14 +31,12 @@ class _LoginScreenState extends State<LoginScreen> {
                 children: [
                   AppTextField(
                     labelText: "Email",
-                    controller: login.loginEmail,
                   ),
                   SizedBox(
                     height: 10,
                   ),
                   AppTextField(
                     labelText: "Password",
-                    controller: login.loginPassword,
                   ),
                 ],
               ),
@@ -43,36 +44,43 @@ class _LoginScreenState extends State<LoginScreen> {
             SizedBox(
               height: 20,
             ),
-            Obx((){
-              return AppButton(
-                title: "Login",
-                loading: login.loading,
-                onTap: (){
-                  login.isLogin(context);
-                },
-              );
-            }),
-            SizedBox(
+            Consumer<LoadingButtonProvider>(
+              builder: (context,value,child){
+                return AppButton(
+                  title: "Login",
+                  loading: value.loading,
+                  onTap: (){
+                    // AuthenticationController()
+                    //     .login(
+                    //   email.text.trim(),
+                    //   password.text.trim(),
+                    // );
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => InformationScreen()));
+                  },
+                );
+              },
+            ),
+            const SizedBox(
               height: 20,
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(
+                const Text(
                   "Don't have any account?",
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
                     fontSize: 16,
                   ),
                 ),
-                SizedBox(
+                const SizedBox(
                   width: 10,
                 ),
                 InkWell(
                   onTap: (){
                     Navigator.push(context, MaterialPageRoute(builder: (context) => SignUpScreen()));
                   },
-                  child: Text(
+                  child: const Text(
                     "Sign up",
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
